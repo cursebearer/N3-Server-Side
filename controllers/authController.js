68c6a803aa82ac41171c21f5fd398cb9c07c5464
cursebearer <rafa.pavesipassos@gmail.com>
2024-06-25 23:48:01 -0300

    feat: console.logs removidos

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,6 @@ authController.login = async (req, res) => {
     const { email, senha } = req.body;
 
     try {
-        // Busca o interessado pelo e-mail
         const interessado = await interessadoModel.listarByEmail(email);
 
         if (!interessado) {
@@ -15,18 +14,11 @@ authController.login = async (req, res) => {
             return res.status(404).json({ error: 'Interessado não encontrado' });
         }
 
-        console.log('Interessado encontrado:', interessado);
-
-        console.log('Senha fornecida:', senha);
-        console.log('Senha do banco (texto simples):', interessado.senha);
-
-        // Comparação direta das senhas
         if (senha !== interessado.senha) {
             console.log('Senha incorreta');
             return res.status(400).json({ error: 'Senha incorreta' });
         }
 
-        // Gera um token JWT com o ID do interessado
         const token = jwt.sign({ id: interessado.id_interessado }, 'chave_secreta_do_token', { expiresIn: '1h' });
 
         res.json({ token });
